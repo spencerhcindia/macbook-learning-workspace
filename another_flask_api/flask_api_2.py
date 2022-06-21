d7c44be65012caf55481508c48e7f12327ffc967
Spencer Cindia <spencer@Spencers-MacBook-Pro.local>
2022-06-21 19:22:54 -0400

    now get searches list and returns value of key

--- another_flask_api/flask_api_2.py
+++ another_flask_api/flask_api_2.py
@@ -4,11 +4,18 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {"data": "Hello World"}
+names = {"spencer": {"age": 28, "gender": "male"}, 
+         "tim": {"age": 19, "gender": "male"}}
 
-api.add_resource(HelloWorld, "/hello")
+class HelloWorld(Resource):
+    def get(self, name):
+        return names[name]
+
+    def post(self):
+        return {"data": "posted"}
+
+
+api.add_resource(HelloWorld, "/hello/<string:name>")
 
 if __name__ == "__main__":
     app.run(debug=True)
