531916822db728887bf14b1ff2ec9c655099e010
Spencer Cindia <spencer@Spencers-MacBook-Pro.local>
2022-06-22 11:36:02 -0400

    fixed 500 error, abort method just takes the message string, not message=. changed put to post, and added delete

--- another_flask_api/flask_api_2.py
+++ another_flask_api/flask_api_2.py
@@ -13,7 +13,11 @@ videos = {}
 
 def abort_if_id_doesnt_exist(video_id):
     if video_id not in videos:
-        abort(404, message="Video id is not valid")
+        abort(404, "Video id is not valid")
+
+def abort_if_video_exists(video_id):
+    if video_id in videos:
+        abort(409, "Video already exists")
 
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, required=True, help="Name of the video is required.")
@@ -32,10 +36,15 @@ class Video(Resource):
         abort_if_id_doesnt_exist(video_id)
         return videos[video_id]
     
-    def put(self, video_id):
+    def post(self, video_id):
         args = video_put_args.parse_args()
         videos[video_id] = args
         return videos[video_id], 201
+    
+    def delete(self, video_id):
+        abort_if_id_doesnt_exist(video_id)
+        del videos[video_id]
+        return '', 204
 
 
 api.add_resource(Video, "/video/<int:video_id>")
