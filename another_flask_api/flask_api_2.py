e7d291ccf75a79a661c265bb22496d62ba057100
spencer <spencer@realgrey.com>
2022-06-28 14:33:57 -0400

    intro to react

--- another_flask_api/flask_api_2.py
+++ another_flask_api/flask_api_2.py
@@ -1,40 +1,39 @@
-from ast import arg
-from email import message
 from flask import Flask, request, abort
 from flask_restful import Api, Resource, reqparse
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class VideoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    views = db.Column(db.Integer, nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Video(name={name}, views={views}, likes={likes})"
+
+#comment out after running for the first time
+# db.create_all()
 
 names = {"spencer": {"age": 28, "gender": "male"}, 
          "tim": {"age": 19, "gender": "male"}}
 
-videos = {}
 
-def abort_if_id_doesnt_exist(video_id):
-    if video_id not in videos:
-        abort(404, "Video id is not valid")
-
-def abort_if_video_exists(video_id):
-    if video_id in videos:
-        abort(409, "Video already exists")
 
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, required=True, help="Name of the video is required.")
 video_put_args.add_argument("views", type=int, required=True, help="Views of the video is required.")
 video_put_args.add_argument("likes", type=int, required=True, help="Likes of the video is required.")
 
-class HelloWorld(Resource):
-    def get(self, name):
-        return names[name]
-
-    def post(self):
-        return {"data": "posted"}
 
 class Video(Resource):
     def get(self, video_id):
-        abort_if_id_doesnt_exist(video_id)
-        return videos[video_id]
+        result = VideoModel.query.get(id=video_id)
+        return result 
     
     def post(self, video_id):
         args = video_put_args.parse_args()
