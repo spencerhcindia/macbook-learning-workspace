7c1835d8ca50d34d1cb79a36edb10b175195cb22
spencer <spencer@realgrey.com>
2022-09-17 18:16:23 -0400

    on my way...

--- pygame/first_try/tutorial2.py
+++ pygame/first_try/tutorial2.py
@@ -1,7 +1,16 @@
+from hashlib import new
 import pygame
 import random
 
+pygame.init()
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+
+clock = pygame.time.Clock()
+
 from pygame.locals import (
+    RLEACCEL,
     K_UP,
     K_DOWN,
     K_LEFT,
@@ -14,9 +23,11 @@ from pygame.locals import (
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
-        self.surf = pygame.Surface((75, 25))
-        self.surf.fill("green")
+        self.surf = pygame.image.load("jet.png").convert_alpha()
+        self.surf = pygame.transform.smoothscale(self.surf, (100, 100))
+        self.surf.set_colorkey("white", RLEACCEL)
         self.rect = self.surf.get_rect()
+        self.radius = 25
     
     def update(self, pressed_keys):
         if pressed_keys[K_UP]:
@@ -47,56 +58,96 @@ class Player(pygame.sprite.Sprite):
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
         super(Enemy, self).__init__()
-        self.surf = pygame.Surface((20,10))
-        self.surf.fill(("red"))
+        self.surf = pygame.image.load("bullet.png").convert_alpha()
+        self.surf = pygame.transform.smoothscale(self.surf, (50, 50))
+        self.surf.set_colorkey("white", RLEACCEL)
         self.rect = self.surf.get_rect(
             center=(
                 random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
                 random.randint(0,SCREEN_HEIGHT)
             )
         )
-        self.speed = random.randint(5, 20)
+        self.speed = random.randint(2, 3)
+        self.radius = 15
     
     def update(self):
         self.rect.move_ip(-self.speed, 0)
         if self.rect.right < 0:
             self.kill()
 
+class Cloud(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Cloud, self).__init__()
+        self.surf = pygame.image.load("clound.png").convert_alpha()
+        self.surf = pygame.transform.scale(self.surf, (500, 500))
+        self.surf.set_colorkey(("black"), RLEACCEL)
+        self.rect = self.surf.get_rect(
+            center=(
+                random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
+                random.randint(0, SCREEN_HEIGHT)
+            )
+        )
+    def update(self):
+        self.rect.move_ip(-1, 0)
+        if self.rect.right < 0:
+            self.kill()
 
+
+
+
+
+ADDENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(ADDENEMY, 750)
+ADDCLOUD = pygame.USEREVENT + 2
+pygame.time.set_timer(ADDCLOUD, 1000)
 
 player = Player()
+
 enemies = pygame.sprite.Group()
+clouds = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
 all_sprites.add(player)
 
-pygame.init()
-
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 800
-
-screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-ADDENEMY = pygame.USEREVENT + 1
-pygame.time.set_timer(ADDENEMY, 250)
 
 running = True
+
+
 while running:
     for event in pygame.event.get():
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE:
                 running = False
+            if event.key == K_RIGHT:
+                x1_right = True
+            if event.key == K_LEFT:
+                x1_left = True
+            if event.key == K_DOWN:
+                x1_down = True
+            if event.key == K_UP:
+                x1_up = True
         elif event.type == pygame.QUIT:
             running = False
         elif event.type == ADDENEMY:
             new_enemy = Enemy()
             enemies.add(new_enemy)
             all_sprites.add(new_enemy)
+        elif event.type == ADDCLOUD:
+            new_cloud = Cloud()
+            clouds.add(new_cloud)
+            all_sprites.add(new_cloud)
+        
     
     pressed_keys = pygame.key.get_pressed()
+    
+    if pressed_keys[KEYDOWN] or pressed_keys[K_UP]:
+        pass
+    
     player.update(pressed_keys)
 
     enemies.update()
+    clouds.update()
     
-    screen.fill("white")
+    screen.fill((135, 206, 250))
     surf = pygame.Surface((50,50))
     surf.fill((0,0,0))
     rect = surf.get_rect()
@@ -109,10 +160,15 @@ while running:
     for entity in all_sprites:
         screen.blit(entity.surf, entity.rect)
     
-    if pygame.sprite.spritecollideany(player, enemies):
-        player.kill()
+    hits = pygame.sprite.spritecollide(player, enemies, False, pygame.sprite.collide_circle)
+    if hits:
         running = False
+    
+    # if pygame.sprite.spritecollideany(player, enemies):
+    #     player.kill()
+    #     running = False
 
     pygame.display.flip()
+    # clock.tick(60)
 
 pygame.quit()
